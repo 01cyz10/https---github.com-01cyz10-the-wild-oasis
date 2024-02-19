3d42dd0674314b67fce63bc1c5ef5ccdbb83ac5e
Yuzhe Chen <chenyuzhe0110@outlook.com>
2024-02-19 16:14:02 +1300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,53 @@
+import styled from 'styled-components';
+import GlobalStyles from '../styles/GlobalStyles';
+import Button from '../ui/Button';
+import Input from '../ui/Input';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+
+// only available in this component
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  /* background-color: orange; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World 123</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type='horizontal'>
+            <Heading type='h1'>The Wild Oasis</Heading>
+
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check In</Button>
+              <Button
+                variations='secondary'
+                size='medium'
+                onClick={() => alert('Check out')}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type='number' placeholder='Number of Guests'></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 export default App;

18c7fb3d374e8a2878f05b9542274b58d1b2cf62
Yuzhe Chen <chenyuzhe0110@outlook.com>
2024-03-16 20:33:02 +1300

    commit

--- pages/Cabins.jsx
+++ pages/Cabins.jsx
@@ -10,7 +10,6 @@ function Cabins() {
   return (
     <Row type='horizontal'>
       <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
       <img src='https://qzteqcfttljuntrzpfpa.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg' />
     </Row>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Dashboard from '../pages/Dashboard';
 import Bookings from '../pages/Bookings';
 import Cabins from '../pages/Cabins';
@@ -9,11 +11,19 @@ import Login from '../pages/Login';
 import PageNotFound from '../pages/PageNotFound';
 import GlobalStyle from '../styles/GlobalStyles';
 import AppLayout from '../ui/AppLayout';
-import { Children } from 'react';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
@@ -31,7 +41,7 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
